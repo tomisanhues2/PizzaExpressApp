9c20e45a1a14463d40bd28333b9759bb75fba307
Tomas Sanhueza <tomisanhues2@gmail.com>
2024-10-23 19:33:52 -0300

    init

---
+++ app/src/main/java/com/example/pizzaexpressapp/classes/Foods.java
@@ -0,0 +1,8 @@
+package com.example.pizzaexpressapp.classes;
+
+public class Foods extends AItems {
+
+    public Foods(int id, String name, int image, double price) {
+        super(id, name, image, price);
+    }
+}
---
+++ app/src/main/java/com/example/pizzaexpressapp/classes/IItems.java
@@ -0,0 +1,8 @@
+package com.example.pizzaexpressapp.classes;
+
+public interface IItems {
+    int getId();
+    String getName();
+    int getImage();
+    double getPrice();
+}
